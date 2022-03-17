76f02d79baeceaa01c4fe4d40a7f5bc3eb4a58e5
ekurtgl <66868163+ekurtgl@users.noreply.github.com>
2022-03-17 14:31:58 -0500

    Add files via upload

--- main_read_data.py
+++ main_read_data.py
@@ -4,12 +4,15 @@ import struct
 import numpy as np
 
 controller = Leap.Controller()
+data_path = 'C:\\Users\\emrek\\PycharmProjects\\RadarGui\\data\\'
+fname = data_path + 'frame.data'
 
-with open("frame.data", "rb") as data_file:
+with open(fname, "rb") as data_file:
     # The first 4 bytes of the file to determine how much data to read to get an entire frame
     # https://developer-archive.leapmotion.com/documentation/python/devguide/Leap_Serialization.html
 
     next_block_size = data_file.read(4)
+    cnt = 0
     while next_block_size:
         size = struct.unpack('i', next_block_size)[0]
         data = data_file.read(size)
@@ -20,4 +23,13 @@ with open("frame.data", "rb") as data_file:
         frame = Leap.Frame()
         frame.deserialize((leap_byte_array, size))
         next_block_size = data_file.read(4)
+
+        print('Cnt:' + str(cnt) +
+              ', Frame ID: ' + str(frame.id) +
+              ', Timestamp: ' + str(frame.timestamp) +
+              ', # of Hands: ' + str(len(frame.hands)) +
+              ', # of Fingers: ' + str(len(frame.fingers)) +
+              ', # of Tools: ' + str(len(frame.tools)) +
+              ', # of Gestures: ' + str(len(frame.gestures())))
+        cnt += 1
 
